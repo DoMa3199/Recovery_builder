23493b3acae732bf9bc6cbf82fbef002b86290c2
DoMa3199 <163661012+DoMa3199@users.noreply.github.com>
2025-05-31 20:06:33 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,8 +7,8 @@ export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
 export DEVICE="gta4lve" # codename used in device tree
-export DT_LINK="https://github.com/mrx7014/twrp_device_samsung_gta4lve" # device tree link
-export DT_BRANCH="android-12.1" # device tree branch
+export DT_LINK="https://github.com/DoMa3199/android_device_samsung_gta4lve" # device tree link
+export DT_BRANCH="gta4lvexx-user-11-RP1A.200720.012-T509XXS2AXD1-release-keys" # device tree branch
 export VENDOR="samsung" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="twrp" # recovery type: omni, twrp, something else
